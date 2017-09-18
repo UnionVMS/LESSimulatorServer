f0f32a5ce1444662fc4e73cb25778c2fe35130be
petermcj <petermcj@gmail.com>
2017-09-18 08:58:43 +0200

    use of common-cli for help/port/dnid

--- src/main/java/fish/focus/uvms/simulator/les/server/Main.java
+++ src/main/java/fish/focus/uvms/simulator/les/server/Main.java
@@ -10,167 +10,200 @@ import org.slf4j.LoggerFactory;
 
 import fish.focus.uvms.simulator.les.commandhandler.DNIDHandler;
 import fish.focus.uvms.simulator.les.commandhandler.POLLHandler;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class Main {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-	private static Properties settings;
-	private static Object lock = new Object();
+    private static Properties settings;
+    private static Object lock = new Object();
 
-	/**
-	 * accessible from all modules that needs it OBS reads from file since it is
-	 * immutable
-	 * 
-	 * @return Settings.properties
-	 * @throws IOException
-	 */
-	public static Properties getSettings() throws IOException {
-		synchronized (lock) {
-			return settings;
-		}
-	}
+    /**
+     * accessible from all modules that needs it OBS reads from file since it is immutable
+     *
+     * @return Settings.properties
+     * @throws IOException
+     */
+    public static Properties getSettings() throws IOException {
+        synchronized (lock) {
+            return settings;
+        }
+    }
 
-	/**
-	 * Load properties file from classpath
-	 * 
-	 * @throws IOException
-	 */
-	private static void loadProperties() throws IOException {
+    /**
+     * Load properties file from classpath
+     *
+     * @throws IOException
+     */
+    private static void loadProperties() throws IOException {
 
-		Properties properties = new Properties();
-		InputStream is = Main.class.getClassLoader().getResourceAsStream("settings.properties");
-		if (is == null) {
-			throw new IOException();
-		}
-		properties.load(is);
-		is.close();
-		synchronized (lock) {
-			settings = properties;
-		}
-	}
+        Properties properties = new Properties();
+        InputStream is = Main.class.getClassLoader().getResourceAsStream("settings.properties");
+        if (is == null) {
+            throw new IOException();
+        }
+        properties.load(is);
+        is.close();
+        synchronized (lock) {
+            settings = properties;
+        }
+    }
 
-	/**
-	 * Resolve port if not valid terminates immediately
-	 * 
-	 * @param portStr
-	 * @return port
-	 */
-	private static int resolvePort(String portStr) {
-		try {
-			return Integer.parseInt(portStr);
-		} catch (NumberFormatException nfe) {
-			LOGGER.error("Invalid port");
-			System.exit(-1);
-			throw nfe;
-		}
-	}
+    /**
+     * Resolve port if not valid terminates immediately
+     *
+     * @param portStr
+     * @return port
+     */
+    private static int resolvePort(String portStr) {
+        try {
+            return Integer.parseInt(portStr);
+        } catch (NumberFormatException nfe) {
+            LOGGER.error("Invalid port");
+            System.exit(-1);
+            throw nfe;
+        }
+    }
 
-	/**
-	 * @param args
-	 * @throws IOException
-	 */
-	public static void main(String[] args) {
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) {
 
-		try {
-			loadProperties();
-		} catch (IOException e) {
-			LOGGER.error("settings.properties not found. cannot start");
-			System.exit(-1);
-		}
+        try {
+            loadProperties();
+        } catch (IOException e) {
+            LOGGER.error("settings.properties not found. cannot start");
+            System.exit(-1);
+        }
 
-		String portFromPropertyFile = settings.getProperty("server.port");
+        String portFromPropertyFile = settings.getProperty("server.port");
 
-		String portFromCommandLine = null;
-		String dnidFromCommandLine = null;
-		if (args.length > 0) {
+        String portFromCommandLine = null;
+        String dnidFromCommandLine = null;
 
-			int numberOfArguments = args.length;
-			for (int i = 0; i < numberOfArguments; i++) {
-				String tmpArg = args[i];
-				if (tmpArg.indexOf("=") < 0) {
-					continue;
-				}
-				String[] buf = tmpArg.split("=");
-				if (buf.length > 2) {
-					continue;
-				}
-				String key = buf[0].trim().toLowerCase();
-				String val = buf[1].trim();
+        CommandLineParser parser = new DefaultParser();
 
-				if (key.endsWith("port")) {
-					portFromCommandLine = val;
-				}
-				if (key.endsWith("dnid")) {
-					dnidFromCommandLine = val;
-					settings.put("dnid", dnidFromCommandLine);
-				}
+        Options options = new Options();
 
-			}
-		}
+        options.addOption(Option.builder("h")
+                .longOpt("help")
+                .desc("print this message")
+                .required(false)
+                .build());
 
-		int port = Integer.MIN_VALUE;
-		if (portFromCommandLine != null) {
-			port = resolvePort(portFromCommandLine);
-		} else {
-			port = resolvePort(portFromPropertyFile);
-		}
+        options.addOption(Option.builder("p")
+                .required(false)
+                .longOpt("port")
+                .desc("the telnet port for the simulator")
+                .hasArg()
+                .numberOfArgs(1)
+                .argName("PORT")
+                .build());
 
-		Server server = null;
-		try {
-			server = new Server(port);
+        options.addOption(Option.builder("d")
+                .required(false)
+                .longOpt("dnid")
+                .desc("a semicolon separted list of DNIDS ex '10095,10096'")
+                .hasArg()
+                .valueSeparator(',')
+                .argName("DNID")
+                .build());
 
-		} catch (IOException e) {
-			LOGGER.error("Could not create server. cannot start");
-			System.exit(-1);
-		}
+        try {
+            CommandLine line = parser.parse(options, args);
 
-		// ADD handlers when needed
+            if (line.hasOption("port")) {
+                portFromCommandLine = line.getOptionValue("port");
+            }
 
-		server.registerCommand(new Command("POLL") {
-			@Override
-			public Response handle(List<String> arguments) {
-				if (arguments.size() > 0) {
-					POLLHandler pollHandler = new POLLHandler(arguments);
-					if (pollHandler.verify()) {
-						return pollHandler.execute();
-					} else {
-						return new Response("POLL request not OK");
-					}
-				} else
-					return new Response("No arguments passed in POLL command");
-			}
+            if (line.hasOption("dnid")) {
+                dnidFromCommandLine = line.getOptionValue("dnid");
+                settings.put("dnid", dnidFromCommandLine);
+            }
 
-		});
+            if (line.hasOption("help")) {
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("LesSimulator Server", options);
+                return;
+            }
 
-		server.registerCommand(new Command("DNID") {
-			@Override
-			public Response handle(List<String> arguments) {
-				if (arguments.size() > 0) {
-					DNIDHandler dnidHandler = new DNIDHandler(arguments);
-					if (dnidHandler.verify()) {
-						return dnidHandler.execute();
-					} else {
-						return new Response("DNID request not OK");
-					}
-				} else
-					return new Response("No arguments passed in DNID command");
-			}
+        } catch (ParseException exp) {
+            LOGGER.error("Unexpected exception:", exp);
+            return;
+        }
 
-		});
+        int port = Integer.MIN_VALUE;
+        if (portFromCommandLine != null) {
+            port = resolvePort(portFromCommandLine);
+        } else {
+            port = resolvePort(portFromPropertyFile);
+        }
 
-		server.registerCommand(new Command("quit") {
+        Server server = null;
+        try {
+            server = new Server(port);
 
-			@Override
-			public Response handle(List<String> arguments) {
-				return new Response("bye", false);
-			}
+        } catch (IOException e) {
+            LOGGER.error("Could not create server. cannot start");
+            System.exit(-1);
+        }
 
-		});
+        // ADD handlers when needed
+        server.registerCommand(new Command("POLL") {
+            @Override
+            public Response handle(List<String> arguments) {
+                if (arguments.size() > 0) {
+                    POLLHandler pollHandler = new POLLHandler(arguments);
+                    if (pollHandler.verify()) {
+                        return pollHandler.execute();
+                    } else {
+                        return new Response("POLL request not OK");
+                    }
+                } else {
+                    return new Response("No arguments passed in POLL command");
+                }
+            }
 
-		server.start();
-		String msg = "Server running. Listening on port: " + port + " and dnid: [" + settings.getProperty("dnid") + "]";
-		LOGGER.info(msg);
+        });
 
-	}
+        server.registerCommand(new Command("DNID") {
+            @Override
+            public Response handle(List<String> arguments) {
+                if (arguments.size() > 0) {
+                    DNIDHandler dnidHandler = new DNIDHandler(arguments);
+                    if (dnidHandler.verify()) {
+                        return dnidHandler.execute();
+                    } else {
+                        return new Response("DNID request not OK");
+                    }
+                } else {
+                    return new Response("No arguments passed in DNID command");
+                }
+            }
+
+        });
+
+        server.registerCommand(new Command("quit") {
+
+            @Override
+            public Response handle(List<String> arguments) {
+                return new Response("bye", false);
+            }
+
+        });
+
+        server.start();
+        String msg = "Server running. Listening on port: " + port + " and dnid: [" + settings.getProperty("dnid") + "]";
+        LOGGER.info(msg);
+
+    }
 }
