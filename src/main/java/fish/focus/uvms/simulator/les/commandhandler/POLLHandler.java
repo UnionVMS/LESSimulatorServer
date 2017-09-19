d253e49b6ef053bbf1a8e7f6ebdc00cef1ade616
thomasfanto <thomas.fanto@consid.se>
2017-09-19 09:00:25 +0200

    same

--- src/main/java/fish/focus/uvms/simulator/les/commandhandler/POLLHandler.java
+++ src/main/java/fish/focus/uvms/simulator/les/commandhandler/POLLHandler.java
@@ -1,24 +1,22 @@
 package fish.focus.uvms.simulator.les.commandhandler;
 
-import java.util.List;
-
 import fish.focus.uvms.simulator.les.server.Response;
 
 public class POLLHandler {
 
-	private List<String> arguments;
+	private String arguments;
 
-	public POLLHandler(List<String> arguments) {
+	public POLLHandler(String arguments) {
 		this.arguments = arguments;
 	}
-	
-	public boolean verify(){
-		
+
+	public boolean verify() {
+
 		return true;
 	}
-	
-	public Response execute(){
-		return new Response(arguments.get(0) + " POLL");
+
+	public Response execute() {
+		return new Response(arguments + " POLL");
 	}
 
 }
