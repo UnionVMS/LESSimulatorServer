a9269422a2d614d830821637ed246513dc950e19
thomasfanto <thomas.fanto@consid.se>
2017-09-21 15:18:04 +0200

    examining bytearray activities in body

--- src/main/java/fish/focus/uvms/simulator/les/common/msgencodingsupport/TTTLesEncoder.java
+++ src/main/java/fish/focus/uvms/simulator/les/common/msgencodingsupport/TTTLesEncoder.java
@@ -4,13 +4,15 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Date;
 
+import org.joda.time.DateTime;
+
 public class TTTLesEncoder {
 
 	private byte[] int2ByteArray(int myInteger) {
 		return ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(myInteger).array();
 	}
 
-	private byte[] date2ByteArray(Date aDate) {
+	private byte[] date2ByteArray_INHEADER(Date aDate) {
 		byte[] arr = new byte[4];
 		Long l = aDate.getTime() / 1000;
 		byte[] wrk = ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN).putLong(l).array();
@@ -21,8 +23,14 @@ public class TTTLesEncoder {
 		arr[3] = wrk[3];
 		return arr;
 	}
+	
+	private byte[] double2ByteArray(double myDouble) {
+		return ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN).putDouble(myDouble).array();
+	}
 
-	public byte[] createHeader(int headerType, int refNumber, int dataPresentation, int satelliteId, int messageLength,
+	
+
+	public byte[] createHeader(int headerType, int refNumber, int dataPresentation, int satId, int messageLength,
 			Date storedTime, int dnid, int memberId) {
 
 		byte[] h = new byte[23];
@@ -54,15 +62,14 @@ public class TTTLesEncoder {
 		byte[] dataPresentation_BA = int2ByteArray(dataPresentation);
 		h[10] = dataPresentation_BA[0];
 
-		int satIdHex = Integer.valueOf(String.valueOf(satelliteId), 16);
-		byte[] satelliteId_BA = int2ByteArray(satIdHex);
-		h[11] = satelliteId_BA[0];
+		//h[11] = int2ByteArray(satId)[0];
+		h[11] = 42;
 
 		byte[] messageLength_BA = int2ByteArray(messageLength);
 		h[12] = messageLength_BA[0];
 		h[13] = messageLength_BA[1];
 
-		byte[] storedTime_BA = date2ByteArray(storedTime);
+		byte[] storedTime_BA = date2ByteArray_INHEADER(storedTime);
 		h[14] = storedTime_BA[0];
 		h[15] = storedTime_BA[1];
 		h[16] = storedTime_BA[2];
@@ -81,11 +88,66 @@ public class TTTLesEncoder {
 	}
 	
 	
-	public byte[] createBody(int headerType, int refNumber, int dataPresentation, int satelliteId, int messageLength,
-			Date storedTime, int dnid, int memberId) {
+	private void pack(int dataReportFormat,  Double latitude){
+		
+		/*
+		 * 			String s = Integer.toBinaryString(num);
+			String s2 = String.format("%8s", s);
+			String s3 = s2.replace(' ', '0');
+			int i = Integer.parseInt(s3.substring(0, 2), 2);
+
+		 */
 		
 		
-		return null;
+		String reportFormat = Integer.toBinaryString(dataReportFormat);
+		byte[] latitude_BA = double2ByteArray(latitude);
+		
+		String byte1 = Integer.toBinaryString(latitude_BA[0]);
+		String byte2 = Integer.toBinaryString(latitude_BA[1]);
+		String byte3 = Integer.toBinaryString(latitude_BA[2]);
+		String byte4 = Integer.toBinaryString(latitude_BA[3]);
+		String byte5 = Integer.toBinaryString(latitude_BA[4]);
+		String byte6 = Integer.toBinaryString(latitude_BA[5]);
+		String byte7 = Integer.toBinaryString(latitude_BA[6]);
+		String byte8 = Integer.toBinaryString(latitude_BA[7]);
+
+		
+		
+		System.out.println();
+		
+		
+	}
+	
+	public byte[] createBody(int dataReportFormat, Double latitude, Double longitude, int memCode, int dayOfMonth, int hour, int minutes, DateTime positionDate, Double speed, Double course) {
+		
+		byte[] b = new byte[255];
+		
+		pack(dataReportFormat, latitude);
+		
+		
+		b[0] = int2ByteArray(dataReportFormat)[0];
+		
+		byte[] latitude_BA = double2ByteArray(latitude);
+		
+		byte[] longitude_BA = double2ByteArray(longitude);
+		
+		byte[] memCode_BA = int2ByteArray(memCode);
+
+		byte[] dayOfMonth_BA = int2ByteArray(dayOfMonth);
+		
+		byte[] hour_BA = int2ByteArray(hour);
+		
+		byte[] minutes_BA = int2ByteArray(minutes);
+		
+		
+		
+		byte[] speed_BA = double2ByteArray(speed);
+
+		
+		byte[] course_BA = double2ByteArray(course);
+		
+		
+		return b;
 	}
 
 	
--- src/test/java/fish/focus/uvms/simulator/les/TestTTLesMessageEncoder.java
+++ src/test/java/fish/focus/uvms/simulator/les/TestTTLesMessageEncoder.java
@@ -3,16 +3,68 @@ package fish.focus.uvms.simulator.les;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.joda.time.DateTime;
+import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import fish.focus.uvms.simulator.les.common.msgdecodingsupport.InmarsatBody;
 import fish.focus.uvms.simulator.les.common.msgdecodingsupport.InmarsatHeader;
 import fish.focus.uvms.simulator.les.common.msgencodingsupport.TTTLesEncoder;
 
 public class TestTTLesMessageEncoder {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TestTTLesMessageEncoder.class);
+
+	@Test
+	public void examineWhatIsHappening() {
+
+		int test = 17;
+		byte aByte = (byte) test;
+
+		int result1 = Integer.MIN_VALUE;
+		;
+		int result2 = Integer.MIN_VALUE;
+
+		{ // dissekerad kod
+			String tmp1 = String.format("%02X ", aByte);
+			String tmp2 = tmp1.trim();
+
+			int num = Integer.parseInt(tmp2, 16);
+
+			String s = Integer.toBinaryString(num);
+			String s2 = String.format("%8s", s);
+			String s3 = s2.replace(' ', '0');
+			int i = Integer.parseInt(s3.substring(0, 2), 2);
+
+			LOGGER.info("'" + tmp1 + "'");
+			LOGGER.info("'" + tmp2 + "'");
+			LOGGER.info(num + "");
+			LOGGER.info(s);
+			LOGGER.info(s2);
+			LOGGER.info(s3);
+			result1 = i;
+		}
+		// original code in inmarsatBody
+		{
+			int num = Integer.parseInt(String.format("%02X ", aByte).trim(), 16);
+			String s = Integer.toBinaryString(num);
+			s = String.format("%8s", s).replace(' ', '0');
+			int i = Integer.parseInt(s.substring(0, 2), 2);
+			result2 = i;
+		}
+
+		LOGGER.info(result1 + "");
+		LOGGER.info(result2 + "");
+
+		Assert.assertTrue(result1 == result2);
+
+	}
+
 	@Test
 	public void createHeader() {
-		
+
 		Calendar cal = Calendar.getInstance();
 		cal.set(Calendar.YEAR, 1999);
 		cal.set(Calendar.MONTH, Calendar.DECEMBER);
@@ -24,29 +76,25 @@ public class TestTTLesMessageEncoder {
 		int headerType = 4;
 		int refNumber = 847842;
 		int dataPresentation = 2;
-		int satelliteId = 34; // ?????
+		int satId = 42; // ?????
 		int messageLength = 282;
 		int dnid = 10745;
 		int memberId = 4;
 
-		byte[] header = enc.createHeader(headerType, refNumber, dataPresentation, satelliteId, messageLength,
-				storedTime, dnid, memberId);
+		byte[] header = enc.createHeader(headerType, refNumber, dataPresentation, satId, messageLength, storedTime,
+				dnid, memberId);
 
 		InmarsatHeader iHeader = new InmarsatHeader(header);
-		
+
 		int ht = Integer.parseInt(iHeader.getheaderType());
-		assert(ht == headerType);
-		assert(iHeader.getMsgRefNr() == refNumber);
-		assert(iHeader.getDataPresentation() == dataPresentation);
-		//assert(iHeader.getSatId() == satelliteId);                                   // ???????
-		assert(iHeader.getMessageLength() == messageLength);
-		assert(iHeader.getDnid() == dnid);
-		assert(iHeader.getMemberId() == memberId);
-		
-		
-		
-		
-		
+		Assert.assertTrue(ht == headerType);
+		Assert.assertTrue(iHeader.getMsgRefNr() == refNumber);
+		Assert.assertTrue(iHeader.getDataPresentation() == dataPresentation);
+		// assert(iHeader.getSatId() == satId); // ???????
+		Assert.assertTrue(iHeader.getMessageLength() == messageLength);
+		Assert.assertTrue(iHeader.getDnid() == dnid);
+		Assert.assertTrue(iHeader.getMemberId() == memberId);
+
 		System.out.println(iHeader.toString());
 
 	}
@@ -58,6 +106,47 @@ public class TestTTLesMessageEncoder {
 		InmarsatHeader inmarsatHeader = new InmarsatHeader(header);
 
 		System.out.println(inmarsatHeader.toString());
+
+	}
+
+	@Test
+	public void setDataReportFormat() {
+		byte data = 3;
+		int num = Integer.parseInt(String.format("%02X ", data).trim(), 16);
+		String s = Integer.toBinaryString(num);
+		s = String.format("%8s", s).replace(' ', '0');
+		int i = Integer.parseInt(s.substring(0, 2), 2);
+
+		String bin = Integer.toBinaryString(data);
+		String bin2 = bin.substring(0, 2);
+		byte theByte = Byte.parseByte(bin2, 2);
+
+		System.out.println();
+
+	}
+
+	@Test
+	public void testBody() {
+
+		TTTLesEncoder enc = new TTTLesEncoder();
+
+		int dataReportFormat = 3;
+		Double latitude = 57.500700;
+		Double longitude = 11.491699;
+		int memCode = Integer.MIN_VALUE;
+		int dayOfMonth = 7;
+		int hour = 6;
+		int minutes = 30;
+		DateTime positionDate = DateTime.now();
+		Double speed = 5.0;
+		Double course = 6.0;
+
+		byte[] body = enc.createBody(dataReportFormat, latitude, longitude, memCode, dayOfMonth, hour, minutes,
+				positionDate, speed, course);
+
+		InmarsatBody inmarsatBody = new InmarsatBody(body);
+
+		System.out.println(inmarsatBody.toString());
 
 	}
 
